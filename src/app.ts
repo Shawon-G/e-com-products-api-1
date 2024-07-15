34ffd7a7ca3e96884833d381625412d6d1a9fb8e
ShawonMG <work.shawonmg@gmail.com>
2024-07-15 17:50:01 +0600

    Project Initialized

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+// Parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+console.log(process.cwd());
+// G:\MERN-Bootcamp\e-com-products-api-1.env
+
+export default app;
